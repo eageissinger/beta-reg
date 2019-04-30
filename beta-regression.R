eb236c2c65b7ddb6f8301f7dcd914c19b65daba8
Emilie Geissinger <eageissinger@mun.ca>
2019-04-30 09:56:47 -0230

    added diagnostics for manuscript

--- beta-regression.R
+++ beta-regression.R
@@ -25,10 +25,18 @@ summary(cod)
 m1.fish<-glm(pi~factor(pulse)+year+month,data=na.omit(cod),
         family = gaussian(link="identity"))
 plot(m1.fish)
+hist(resid(m1))
+# plots for manuscript
+plot(x=fitted(m1),y=resid(m1),main=NULL,xlab="Fitted Values",ylab="Residuals")
+hist(resid(m1),main=NULL,xlab="Residuals")
+qqnorm(resid(m1),main=NULL)
+qqline(resid(m1),col='red')
+plot(m1,which=4,main=NULL)
+# model summary/diagnostics
 summary(m1.fish)
 exp(logLik(m1.fish))
 anova(m1.fish)
-
+#ANODEV
 m1.intercept<-glm(pi~1,data=na.omit(cod),
                   family=gaussian(link = "identity"))
 m1.pulse<-glm(pi~1+factor(pulse),data=na.omit(cod),
@@ -44,10 +52,12 @@ m1ANODEV
 # Zero-inflated Beta Regression
 m2.fish<-gamlss(pi~factor(pulse)+year+month,family=BEZI,data=na.omit(cod),trace=F)
 summary(m2.fish)
+#model diagnostics
 plot(m2.fish)
 
 exp(logLik(m2.fish))
 
+# ANODEV
 m2.intercept<-gamlss(pi~1,family=BEZI,data=na.omit(cod0),trace=F)
 m2.pulse<-gamlss(pi~1+factor(pulse),family=BEZI,data=na.omit(cod0),trace=F)
 m2.yr<-gamlss(pi~1+factor(pulse)+year,family=BEZI,data=na.omit(cod0),trace=F)
@@ -56,7 +66,8 @@ m2.month<-gamlss(pi~1+factor(pulse)+year+month,family=BEZI,data=na.omit(cod0),tr
 m2ANODEV<-lrtest(m2.intercept,m2.pulse,m2.yr,m2.month)
 m2ANODEV
 
-# --- glmmTMB with type III ANOVA ----
+# ---- Regular Beta Regression
+# glmmTMB with type III ANOVA 
 cod0<-cod%>%
   filter(pi!=0)
 m3<-glmmTMB(pi~factor(pulse),
@@ -76,10 +87,17 @@ m3ANODEV # off from anova just a little bit
 # Advantages: ANOVA table
 # Disadvantages: no diagnostic plots
 
-# Regular
+# Regular beta regressions {betareg}
 m4<-betareg(pi~factor(pulse),data=cod0)
 plot(m4)
+# diagnostic plots for manuscript
+plot(m2,which=1,type="pearson")
+plot(m2,which=4,type="pearson")
+plot(m2,which=5,type="deviance")
+plot(m2,which=2,type="pearson")
+# model summary
 summary(m4)
+#ANODEV
 m4.intercept<-betareg(pi~1,data=cod0)
 m4.pulse<-betareg(pi~1+factor(pulse),data=cod0)
 
