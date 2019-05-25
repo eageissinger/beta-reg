0da7edfa504470a1fdd03ddc6398687fece71621
Emilie Geissinger <eageissinger@mun.ca>
2019-05-25 14:54:49 -0230

    added model comparison calculations and summary tables

--- beta-regression.R
+++ beta-regression.R
@@ -6,6 +6,11 @@ setwd("C:/Users/user/Documents/Research/Beta/")
 # load data
 cod<-read.csv("beta-fish.csv")
 
+install.packages('betareg')
+install.packages('gamlss')
+install.packages('glmmTMB')
+install.packages('DHARMa')
+
 # load packages
 library(betareg)
 library(car)
@@ -16,7 +21,7 @@ library(lmtest)
 library(glmmTMB)
 library(DHARMa)
 
-# ----- Fish -----
+# ----- Fish models -----
 # explore data
 head(cod)
 summary(cod)
@@ -25,17 +30,19 @@ summary(cod)
 m1.fish<-glm(pi~factor(pulse)+year+month,data=na.omit(cod),
         family = gaussian(link="identity"))
 plot(m1.fish)
-hist(resid(m1))
+hist(resid(m.fish1))
 # plots for manuscript
-plot(x=fitted(m1),y=resid(m1),main=NULL,xlab="Fitted Values",ylab="Residuals")
-hist(resid(m1),main=NULL,xlab="Residuals")
-qqnorm(resid(m1),main=NULL)
-qqline(resid(m1),col='red')
-plot(m1,which=4,main=NULL)
+plot(x=fitted(m1.fish),y=resid(m1.fish),main=NULL,xlab="Fitted Values",ylab="Residuals")
+hist(resid(m1.fish),main=NULL,xlab="Residuals")
+qqnorm(resid(m1.fish),main=NULL)
+qqline(resid(m1.fish),col='red')
+plot(m1.fish,which=4,main=NULL)
+
 # model summary/diagnostics
 summary(m1.fish)
 exp(logLik(m1.fish))
 anova(m1.fish)
+
 #ANODEV
 m1.intercept<-glm(pi~1,data=na.omit(cod),
                   family=gaussian(link = "identity"))
@@ -53,11 +60,13 @@ m1ANODEV
 m2.fish<-gamlss(pi~factor(pulse)+year+month,family=BEZI,data=na.omit(cod),trace=F)
 summary(m2.fish)
 #model diagnostics
-plot(m2.fish)
+plot.gamlss(m2.fish)
+hist(resid(m2.fish))
 
-exp(logLik(m2.fish))
 
-# ANODEV
+
+par(mfrow=c(1,1), bg = rgb(232,245,252, maxColorValue = 255))
+#ANODEV
 m2.intercept<-gamlss(pi~1,family=BEZI,data=na.omit(cod0),trace=F)
 m2.pulse<-gamlss(pi~1+factor(pulse),family=BEZI,data=na.omit(cod0),trace=F)
 m2.yr<-gamlss(pi~1+factor(pulse)+year,family=BEZI,data=na.omit(cod0),trace=F)
@@ -65,8 +74,56 @@ m2.month<-gamlss(pi~1+factor(pulse)+year+month,family=BEZI,data=na.omit(cod0),tr
 
 m2ANODEV<-lrtest(m2.intercept,m2.pulse,m2.yr,m2.month)
 m2ANODEV
+# ---- Model comparison: fish ----
+glm.model.fish<-as.data.frame(m1ANODEV)%>%
+  rename(numDf='#Df')
 
-# ---- Regular Beta Regression
+glm.model.fish%>%
+  mutate(Deviance=2*LogLik)%>%
+  mutate(dDeviance=Deviance-lag(Deviance))%>%
+  mutate(LR=exp(dDeviance/2))%>%
+  mutate(AIC=dDeviance-2*numDf)%>%
+  mutate(LR_Df=LR/Df)%>%
+  mutate(dAIC=AIC-lag(AIC))
+
+beta.model.fish<-as.data.frame(m2ANODEV)%>%
+  rename(numDf='#Df')
+beta.model.fish%>%
+  mutate(Deviance=2*LogLik)%>%
+  mutate(dDeviance=Deviance-lag(Deviance))%>%
+  mutate(LR=exp(dDeviance/2))%>%
+  mutate(AIC=dDeviance-2*numDf)%>%
+  mutate(LR_Df=LR/Df)%>%
+  mutate(dAIC=AIC-lag(AIC))
+
+# ---- Model parameters: fish ----
+m1.fish$coefficients
+summary(m1.fish)
+m1.fish$coefficients
+
+summary(m2.fish)
+betacoef<-m2.fish$mu.coefficients
+odds<-exp(betacoef)
+beta.prob<-odds/(1+odds)
+
+parameter.summary<-data.frame(glm=m1.fish$coefficients,
+                              beta=beta.prob)
+
+# ---- Fit statistics: fish -----
+fit.fish<-data.frame(Fit.statistics=c('AIC','LR','Residual Deviance'),
+                     glm=c(AIC(m1.fish),exp(logLik(m1.fish)),
+                           sum(m1.fish$residuals^2)/m1.fish$df.residual),
+                     beta=c(AIC(m2.fish),exp(logLik(m2.fish)),
+                            sum(m2.fish$residuals^2)/m2.fish$df.residual))
+
+
+sum(m1.fish$residuals^2)/m1.fish$df.residual
+
+summary(m2.fish)
+sum(m2.fish$residuals^2)/m2.fish$df.residual
+
+
+# ---- Regular Beta Regression -----
 # glmmTMB with type III ANOVA 
 cod0<-cod%>%
   filter(pi!=0)
@@ -88,8 +145,8 @@ m3ANODEV # off from anova just a little bit
 # Disadvantages: no diagnostic plots
 
 # Regular beta regressions {betareg}
-m4<-betareg(pi~factor(pulse),data=cod0)
-plot(m4)
+m2<-betareg(pi~factor(pulse),data=cod0)
+plot(m2)
 # diagnostic plots for manuscript
 plot(m2,which=1,type="pearson")
 plot(m2,which=4,type="pearson")
