9b80bc8fca8b672640e693b1d434d54831b56bce
icrichmond <icrichmond@mun.ca>
2021-07-23 13:12:31 -0400

    Updating script to evaluate arcsin transformed GLM

--- scripts/2b-NMR-alkyl.R
+++ scripts/2b-NMR-alkyl.R
@@ -1,81 +1,88 @@
-# Beta Regression Analysis 
+# Alkyl Carbon Analyses
+# Comparing beta regression to general linear model with arcsin transformation. 
+# Using alkyl carbon in soil - modelling region and soil layer
+# comparing models with likelihood ratios, residual deviance/residual, and AIac.
 
-# set working directory
-setwd("~/Desktop/Beta Regression Paper/Beta Regression R analysis")
+#### Packages ####
+# load required packages 
+p <- c("betareg", "lmtest", "DescTools", "dplyr")
+lapply(p, library, character.only = T)
 
+#### Data #### 
 # load data
-SoilNMR<-read.csv("NMR data.csv")
+soil <- read.csv("input/Beta_NMRdata.csv")
+# select relevant columns
+soilac <- select(soil, c(Region, Layer, AC))
+# add carbonyl carbon data with arcsin square root transformation
+soilac <- mutate(soilac, AC_arc = asin(sqrt(soilac$AC)))
 
-# load packages
-library(betareg)
-library(car)
-library(gamlss)
-library(emmeans)
-library(dplyr)
-library(lmtest)
-library(glmmTMB)
-library(DHARMa)
--------------------------------------ALKYL CARBON ANALYSES------------------------------------
-
-  # ----- Soil NMR -----
-# explore data
-head(SoilNMR)
-summary(SoilNMR)
-
-# General Linear Model --- Alkyl Carbon
-m4.SoilNMR<-glm(AC~Region+Layer+Region*Layer,data=na.omit(SoilNMR),
-                family = gaussian(link="identity"))
-plot(m4.SoilNMR)
-hist(resid(m4.SoilNMR))
-# plots for manuscript
-plot(x=fitted(m4.SoilNMR),y=resid(m4.SoilNMR),main=NULL,xlab="Fitted Values",ylab="Residuals")
-hist(resid(m4.SoilNMR),main=NULL,xlab="Residuals")
-qqnorm(resid(m4.SoilNMR),main=NULL)
-qqline(resid(m4.SoilNMR),col='red')
-plot(m4.SoilNMR,which=4,main=NULL)
-# model summary/diagnostics
-summary(m4.SoilNMR)
-exp(logLik(m4.SoilNMR))
-anova(m4.SoilNMR)
-#ANODEV
-m4.intercept<-glm(AC~1,data=na.omit(SoilNMR),
-                  family=gaussian(link = "identity"))
-m4.OHCC<-glm(AC~1+Region,data=na.omit(SoilNMR),
-             family=gaussian(link = "identity"))
-m4.CR<-glm(AC~1+Region+Layer,data=na.omit(SoilNMR),
-           family=gaussian(link = "identity"))
-m4.OHCCCR<-glm(AC~1+Region+Layer+Region*Layer,data=na.omit(SoilNMR),
-               family=gaussian(link = "identity"))
-m4ANODEV<-lrtest(m4.intercept,m4.OHCC,m4.CR,m4.OHCCCR)
-m4ANODEV
-
-# Regular beta regressions {betareg}
-library(betareg)
-m6.SoilNMR<-betareg(AC~Region+Layer+Region*Layer,data=SoilNMR)
-plot(m6.SoilNMR)
+#### General Linear Model with Arcsin Transformation ####
+m1 <- glm(AC_arc ~ Region+Layer+Region*Layer,data=na.omit(soilac))
+m1FrameNormal<-cbind(soilac,residuals(m1),fitted(m1))
 # diagnostic plots for manuscript
-plot(m6.SoilNMR,which=1,type="pearson")
-plot(m6.SoilNMR,which=4,type="pearson")
-plot(m6.SoilNMR,which=5,type="deviance")
-plot(m6.SoilNMR,which=2,type="pearson")
-# model summary
-summary(m6.SoilNMR)
+png("figures/glm_alkyl-carbon_diagnostics.png", width = 160, height = 160, units = "mm",res = 600)
+par(mfrow=c(2,2))
+plot(x=fitted(m1),y=resid(m1),main=NULL,
+     xlab="Fitted Values",ylab="Residuals",cex.lab=1.15)
+mtext("A",side=2,line=2,at=0.06,col='black',font=2,las=1,size=1.75)
+hist(resid(m1),main=NULL,xlab="Residuals",
+     cex.lab=1.15)
+mtext("B",side=2,line=2,at=9,col='black',font=2,las=1,size=1.75)
+qqnorm(resid(m1),main=NULL,cex.lab=1.15)
+mtext("C",side=2,line=2,at = 0.06,col='black',font=2,las=1,size=1.75)
+qqline(resid(m1),col='red')
+plot(m1,which=4,caption = NULL,main = NULL,cex.lab=1.15)
+mtext("D",side=2,line=2,at=0.3,col='black',font=2,las=1,size=1.75)
+dev.off()
+# model summary and fit statistics
+summary(m1)
+exp(logLik(m1))
+anova(m1)
 #ANODEV
-m6.intercept<-betareg(AC~1,data=SoilNMR)
-m6.OHCC<-betareg(AC~1+Region,data=SoilNMR)
-m6.CR<-betareg(AC~1+Region+Layer,data=SoilNMR)
-m6.OHCCCR<-betareg(AC~1+Region+Layer+Region*Layer,data=SoilNMR)
+m1.intercept<-glm(AC_arc~1,data=na.omit(soilac))
+m1.OHac<-glm(AC_arc~1+Region,data=na.omit(soilac))
+m1.CR<-glm(AC_arc~1+Region+Layer,data=na.omit(soilac))
+m1.OHacCR<-glm(AC_arc~1+Region+Layer+Region*Layer,data=na.omit(soilac))
+m1ANODEV<-lrtest(m1.intercept,m1.OHac,m1.CR,m1.OHacCR)
+m1ANODEV
 
-install.packages("lrtest")
-m6ANODEV<-lrtest(m6.intercept,m6.OHCC,m6.CR,m6.OHCCCR)
-m6ANODEV # same result as the GLMMTMB
+#### Beta Regression ####
+m2 <- betareg(AC~Region+Layer+Region*Layer,data=soilac)
+m2FrameBeta<-cbind(soilac,residuals(m2),fitted(m2))
+# diagnostic plots for manuscript
+png("figures/beta_alkyl-carbon_diagnostics.png",  width = 160, height = 160, units = "mm",res = 600)
+par(mfrow=c(2,2))
+plot(x=fitted(m2),y=resid(m2),main=NULL,
+     xlab="Fitted Values",ylab="Residuals",cex.lab=1.15, type="pearson")
+mtext("A",side=2,line=2,at=4,col='black',font=2,las=1,size=1.75)
+plot(m2,which = 4, caption=NULL, cex.lab=1.15, type="pearson") 
+mtext("B",side=2,line=2,at=3.5,col='black',font=2,las=1,size=1.75)
+plot(m2, which = 5, caption = NULL, type="deviance", cex.lab=1.15)
+mtext("C",side=2,line=2,at=3.5,col='black',font=2,las=1,size=1.75)
+plot(m2,which=2, type="pearson",caption=NULL, cex.lab=1.15)
+mtext("D",side=2,line=2,at=0.43,col='black',font=2,las=1,size=1.75)
+dev.off()
+# model summary and fit statistics
+summary(m2)
+AIC(m2)
+exp(logLik(m2))
+res.dev<-residuals(m2,type = "deviance")
+(sum(res.dev^2))/109
+sum(res.dev)/109
+#ANODEV
+m2.intercept<-betareg(AC~1,data=soilac)
+m2.OHac<-betareg(AC~1+Region,data=soilac)
+m2.CR<-betareg(AC~1+Region+Layer,data=soilac)
+m2.OHacCR<-betareg(AC~1+Region+Layer+Layer*Region,data=soilac)
+m2ANODEV<-lrtest(m2.intercept,m2.OHac,m2.CR,m2.OHacCR)
+m2ANODEV
 
-
-# ---- Model comparison: SoilNMR ----
-glm.model.SoilNMR.AC<-as.data.frame(m4ANODEV)%>%
+#### Model Comparison ####
+# ANOVA Table GLM
+glm.model.ac<-as.data.frame(m1ANODEV)%>%
   rename(numDf='#Df')
-
-glm.SoilNMR<-glm.model.SoilNMR.AC%>%
+# ANOVA with parameter comparison for GLM
+acglm.soilac<-glm.model.ac%>%
   mutate(Deviance=2*LogLik)%>%
   mutate(dDeviance=Deviance-lag(Deviance))%>%
   mutate(LR=exp(dDeviance/2))%>%
@@ -83,9 +90,11 @@ glm.SoilNMR<-glm.model.SoilNMR.AC%>%
   mutate(LR_Df=LR/Df)%>%
   mutate(dAIC=AIC-lag(AIC))
 
-beta.model.SoilNMR.AC<-as.data.frame(m6ANODEV)%>%
+# ANOVA Table Beta
+beta.model.ac<-as.data.frame(m2ANODEV)%>%
   rename(numDf='#Df')
-beta.SoilNMR<-beta.model.SoilNMR.AC%>%
+# ANOVA with parameter comparison for beta
+acbeta.soilac <-beta.model.ac%>%
   mutate(Deviance=2*LogLik)%>%
   mutate(dDeviance=Deviance-lag(Deviance))%>%
   mutate(LR=exp(dDeviance/2))%>%
@@ -94,34 +103,30 @@ beta.SoilNMR<-beta.model.SoilNMR.AC%>%
   mutate(dAIC=AIC-lag(AIC))
 
 # save tables
-write.csv(glm.SoilNMR,file ="AC.glm.model.csv",row.names=FALSE)
-write.csv(beta.SoilNMR,file ="AC.beta.model.csv",row.names = FALSE)
+write.csv(acglm.soilac,file ="output/glm_alkyl-carbon_model.csv",row.names=FALSE)
+write.csv(acbeta.soilac,file ="output/beta_alkyl-carbon_model.csv",row.names = FALSE)
 
-# ---- Model parameters: SoilNMR ----
-m4.SoilNMR$coefficients
-summary(m4.SoilNMR)
-m4.SoilNMR$coefficients
+#### Model Parameters ####
+m1$coefficients
+summary(m1)
 
-summary(m6.SoilNMR)
-betacoef.AC<-m6.SoilNMR$mu.coefficients
-odds.AC<-exp(coef(m6.SoilNMR))
-beta.prob.AC<-odds/(1+odds)
+summary(m2)
+betacoef<-m2$coefficients
+odds<-exp(betacoef$mean)
+beta.prob<-odds/(1+odds)
 
-parameter.summary.AC<-data.frame(glm=m4.SoilNMR$coefficients,
-                              beta=beta.prob.AC[-c(13)])
-
-# save table
-write.csv(parameter.summary,"AC.parameters.csv",row.names = FALSE)
-
-# ---- Fit statistics: SoilNMR -----
-fit.SoilNMR<-data.frame(Fit.statistics=c('AIC','LR','Residual Deviance'),
-                        glm=c(AIC(m4.SoilNMR),exp(logLik(m4.SoilNMR)),
-                              sum(m4.SoilNMR$residuals^2)/m4.SoilNMR$df.residual),
-                        beta=c(AIC(m6.SoilNMR),exp(logLik(m6.SoilNMR)),
-                               sum(m6.SoilNMR$residuals^2)/m6.SoilNMR$df.residual))
+parameter.summary<-data.frame(glm=m1$coefficients,
+                              beta=beta.prob)
 
 # save table
-write.csv(fit.SoilNMR,"Alkylcarbonfit.stats.csv",row.names = FALSE)
+write.csv(parameter.summary,"output/alkyl-carbon_parameters.csv",row.names = FALSE)
 
+#### Fit Statistics ####
+fit.soilac<-data.frame(Fit.statistics=c('AIC','LR','Residual Deviance'),
+                       glm=c(AIC(m1),exp(logLik(m1)),
+                             sum(m1$residuals^2)/m1$df.residual),
+                       beta=c(AIC(m2),exp(logLik(m2)),
+                              sum(m2$residuals^2)/m2$df.residual))
 
-
+# save table
+write.csv(fit.soilac,"alkyl-carbon_fit.csv",row.names = FALSE)
